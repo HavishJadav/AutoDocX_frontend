a1a74f8d12524991fe8ba03ad67d4db2502d0666
harshit garg <144602034+harshit2414@users.noreply.github.com>
2025-04-02 12:14:52 +0530

    Add files via upload
    login signup page added

---
+++ src/pages/script.js
@@ -0,0 +1,15 @@
+export function handleFormToggle() {
+    const container = document.querySelector('.container');
+    const registerBtn = document.querySelector('.register-btn');
+    const loginBtn = document.querySelector('.login-btn');
+
+    if (registerBtn && loginBtn && container) {
+        registerBtn.addEventListener('click', () => {
+            container.classList.add('active');
+        });
+
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove('active');
+        });
+    }
+}
